4c60c79489ea7c2c682438d68bba4671d881f9e1
Tyler Gaisford <tyler.gaisford@gmail.com>
2023-03-30 14:27:44 -0600

    Completed

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+﻿using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,23 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)] // ← you will need to put some test data and expected result here.
+        [InlineData("car", false)]
+        [InlineData("hanna",false)]
+        [InlineData("hannah", true)]
+        [InlineData("Elle", true)]
+        [InlineData("Renner", true)]
+        public void IsAPalindrome(string nameHolder, bool expected)
         {
+            // Arrange
+            var challenger = new WordSmith();
 
+            // Act
+            bool actual = challenger.IsAPalindrome(nameHolder);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
